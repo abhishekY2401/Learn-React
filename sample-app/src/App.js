fe3d66482228bbfd9f888c7d9b7f712d38d6995a
abhishekY2401 <abhiyd2401@gmail.com>
2022-01-28 20:04:46 +0530

    rendered 3 todo components

--- sample-app/src/App.js
+++ sample-app/src/App.js
@@ -1,7 +1,17 @@
+import Todo from "./Todo";
+
 
 function App() {
   return (
-    <div>Hello, there!</div>
+    <div className="container">
+      <h1>My Todos</h1>
+      <div className="cards">
+        <Todo title="Learn React" />
+        <Todo title="Learn Operating System" />
+        <Todo title="Learn Computer Networks" />
+        <Todo title="Learn Computer Architecture" />
+      </div>
+    </div>
   );
 }
 
