129101f9c28a587d110863531e3b9a083563214d
abhishekY2401 <abhiyd2401@gmail.com>
2022-03-10 23:00:20 +0530

    Added book component

--- sample-app/src/index.js
+++ sample-app/src/index.js
@@ -1,12 +1,46 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
 
+// stateless functional components
+// always returns JSX
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+function BookList() {
+  return (
+    <section>
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+    </section>
+  );
+}
+
+const Book = () => {
+  return (
+    <article>
+      <Image />
+      <Title />
+      <Author />
+    </article>
+  );
+};
+
+const Image = () => (
+  <img
+    src="https://images-na.ssl-images-amazon.com/images/I/41yzMkY-+vL._AC_SX184_.jpg"
+    alt="book"
+  ></img>
 );
+
+const Title = () => {
+  return (
+    <h1>
+      Life Force: How New Breakthroughs in Precision Medicine Can Transform the
+      Quality of Your Life & Those You Love
+    </h1>
+  );
+};
+
+const Author = () => <p>Peter H. Diamandis, Robert Hariri</p>;
+
+ReactDOM.render(<BookList />, document.getElementById("root"));
