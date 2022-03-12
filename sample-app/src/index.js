872d52bd5d34d3a76e67bb12531dbd043eea1bfb
abhishekY2401 <abhiyd2401@gmail.com>
2022-03-12 23:59:59 +0530

    Rendered App component

--- sample-app/src/index.js
+++ sample-app/src/index.js
@@ -1,46 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import App from "./App";
 
-// stateless functional components
-// always returns JSX
-
-function BookList() {
-  return (
-    <section>
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-    </section>
-  );
-}
-
-const Book = () => {
-  return (
-    <article>
-      <Image />
-      <Title />
-      <Author />
-    </article>
-  );
-};
-
-const Image = () => (
-  <img
-    src="https://images-na.ssl-images-amazon.com/images/I/41yzMkY-+vL._AC_SX184_.jpg"
-    alt="book"
-  ></img>
-);
-
-const Title = () => {
-  return (
-    <h1>
-      Life Force: How New Breakthroughs in Precision Medicine Can Transform the
-      Quality of Your Life & Those You Love
-    </h1>
-  );
-};
-
-const Author = () => <p>Peter H. Diamandis, Robert Hariri</p>;
-
-ReactDOM.render(<BookList />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
